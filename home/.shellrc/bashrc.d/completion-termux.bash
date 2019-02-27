7116280f0e30977f10ff31cd164b2c9f16bbc1e0
Hugues Chabot <hugues.chabot@gmail.com>
2019-02-26 21:04:45 -0500

    Add autocompletion and Termux config

---
+++ home/.shellrc/bashrc.d/completion-termux.bash
@@ -0,0 +1,3 @@
+if [ -f $PREFIX/share/bash-completion/bash_completion ]; then
+  source $PREFIX/share/bash-completion/bash_completion
+fi
---
+++ home/.shellrc/bashrc.d/fzf-termux.bash
@@ -0,0 +1,7 @@
+if [ -f $PREFIX/share/fzf/key-bindings.bash ]; then
+  source $PREFIX/share/fzf/key-bindings.bash
+fi
+
+if [ -f $PREFIX/share/fzf/completion.bash ]; then
+  source $PREFIX/share/fzf/completion.bash
+fi
